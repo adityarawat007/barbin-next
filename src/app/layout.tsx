30feaf262e6220696bbe9a87f608c445a2928e36
Aditya <rawataditya795@gmail.com>
2025-09-07 20:52:17 +0530

    fix: icon change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  icons: {
-    icon: "../../public/favicon.ico",
-  },
   openGraph: {
     title: "Barbin Furniture",
     description:
